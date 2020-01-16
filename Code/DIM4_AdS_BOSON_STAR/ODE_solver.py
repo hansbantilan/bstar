0a840e2b1e168d8d0375a2fb3ac1f20daf479d0b
Thomas <thomashelfer@live.de>
2020-01-16 14:09:17 -0500

    Improved Solver

--- Code/DIM4_AdS_BOSON_STAR/ODE_solver.py
+++ Code/DIM4_AdS_BOSON_STAR/ODE_solver.py
@@ -6,7 +6,7 @@ matplotlib.use("agg")
 import os 
 import matplotlib.pyplot as plt
 
-def eqns(y, r):
+def eqns(y, r, D, Lambda):
     """ Differential equation for scalar fields 
 
     Parameters:
@@ -17,8 +17,7 @@ def eqns(y, r):
         dydr (list with reals): derivative for y in r 
 
     """
-    D = 5.0 
-    Lambda = -0.1
+    D = float(D)
     edelta, m, phi, pi = y
     # Where edelta  = e^{-\delta}
 
@@ -38,11 +37,13 @@ def eqns(y, r):
 
 # solve
 
-def shoot(alpha0_guess,phi0,r):
+def shoot(edelta_guess,phi0,D,Lambda,r):
     """ Solved differential equation for shooting process.
 
     Parameters:
-        alpha0_guess (real): The lapse value guess at r = rmin 
+        edelta_guess (real): The lapse value guess at r = rmin 
+        D (int) :   Dimension of the problem
+        Lambda (real) : cosmological constant 
 	phi0 (real) : The phi value at r = rmin
 
     Returns:
@@ -51,19 +52,21 @@ def shoot(alpha0_guess,phi0,r):
     """
     
     # Define initial data vector 
-    y0 = [alpha0_guess,0,phi0,0]
+    y0 = [edelta_guess,0,phi0,0]
     # Solve differential equaion 
-    sol = spi.odeint(eqns, y0, r)
+    sol = spi.odeint(eqns, y0, r, args = (D,Lambda))
     phi_end = sol[-1,2]	
     
     return phi_end
 
-def radial_walker(alpha0_guess,phi0,rstart,rend,deltaR,N): 
+def radial_walker(edelta_guess,phi0,D,Lambda,rstart,rend,deltaR,N,eps): 
     """ Performs shooting for multiple radii rmax shooting process.
 
     Parameters:
-        alpha0_guess (real) : alpha guess for rmin calculation 
-        phi0 (real) : phi value at r = 0 
+        edelta_guess (real) : alpha guess for rmin calculation 
+        phi0 (real) : phi value at r = 0 `
+        D (int) :   Dimension of the problem
+        Lambda (real) : cosmological constant 
         rstart (real) : first rmax for which shooting is performed
 	rend (real) : maximum rmax for which shooting is performed
 	deltaR (real) : stelpsize
@@ -72,14 +75,13 @@ def radial_walker(alpha0_guess,phi0,rstart,rend,deltaR,N):
     Returns:
         alpha0 (real):. alpha0 for rmax   
     """
-    eps = 1e-10 # distance from zero
     range_list = np.arange(rstart,rend,deltaR)
-    alpha0 = alpha0_guess
+    alpha0 = edelta_guess
 
     for R in range_list:
         r = np.linspace(eps, R, N)
 
-        fun = lambda x: shoot(x,phi0,r)
+        fun = lambda x: shoot(x,phi0,D,Lambda,r)
         root = opi.root(fun,alpha0)
         alpha0 = root.x 
 
@@ -88,29 +90,79 @@ def radial_walker(alpha0_guess,phi0,rstart,rend,deltaR,N):
     
     return alpha0[0]
 
-# We want to evaluate the system on 30 linearly
-# spaced times between t=0 and t=3.
+def normalise_edelta(sol): 
+    """ Extractsomega for edelta by the coordinate transformation  t -> omega t 
 
+    Parameters:
+        sol (real array) : were the sol[:,1] corresponds to edelta^(-1) and
+                           and asymtotic value that does not go to 1 
+    Returns:
+        omega (real): frequency of scalar field 
+        sol (real array) : sol array with fixed edelta
+    """
+    edelta = 1./sol[:,0]
+    N = len(edelta)
+    omega = edelta[N-1]
+    edelta = edelta/omega
+    sol[:,0] = 1./edelta
+    return omega , sol 
 
-phi0 = 0.04
-# Resolution of diff eqn 
-Rstart = 15
+def make_filesystem(phi0,D,Lambda):
+    """ Creates Folder for current physics problem if they do not yet exist
+        
+    Parameters:
+        phi0 (real) : phi value at r = 0 `
+        D (int) :   Dimension of the problem
+        Lambda (real) : cosmological constant 
+    Returns:
+        path (string) : path to current file
+
+    """
+
+    name_Lambda_D = "Lambda"+str(Lambda)+"D"+str(D)
+    if not os.path.exists(name_Lambda_D):
+        os.mkdir(name_Lambda_D)
+
+    name_phi = "phi"+str(phi0)
+    if not os.path.exists(name_Lambda_D + "/"+name_phi):
+        os.mkdir(name_Lambda_D + "/"+name_phi)
+    
+    path = name_Lambda_D + "/"+name_phi
+
+    return path 
+
+#=====================
+#  All imporntnat definitions 
+#=====================
+
+# Physics defintions 
+phi0 = 0.04         # centeral phi
+D = 5.0             # Dimension (total not only spacial)
+Lambda = -0.1       # Cosmological constant  
+# Solver definitions 
+Rstart = 3
 Rend = 50.00
 deltaR = 1
 N = 100000
-alpha0_guess = 0.75
+edelta_guess = 0.75
 
+eps = 1e-10 # Small epsilon to avoid r \neq 0 
+ 
+# ====================================
+#   Main routine 
+# ====================================
 
-alpha0 = radial_walker(alpha0_guess,phi0,Rstart,Rend,deltaR,N)
+alpha0 = radial_walker(edelta_guess,phi0,D,Lambda,Rstart,Rend,deltaR,N,eps)
 
-name = "phi"+str(phi0)
-if not os.path.exists(name):
-    os.mkdir(name)
+#=====================================
+#   Output and plotting
+#=====================================
 
-
-r = np.linspace(1e-10, Rend, N)
+r = np.linspace(eps, Rend, N)
 y0 = [ alpha0, 0 ,phi0,0]
-sol = spi.odeint(eqns, y0, r)
+sol = spi.odeint(eqns, y0, r,args = (D,Lambda))
+
+omega,sol =  normalise_edelta(sol) 
 
 plt.plot(r, 1/sol[:, 0], 'b', label='edelta(t)')
 plt.plot(r, 1+sol[:, 1], 'g', label='1+m(t)')
@@ -120,9 +172,13 @@ plt.xlabel('t')
 plt.ylim([0.99,max(1+sol[:,2])*1.2])
 plt.grid()
 
-plt.savefig(name+"/overview.png")
+# ===============================
+path = make_filesystem(phi0,D,Lambda)
 
-np.savetxt(name+"/edelta.dat",1/sol[:, 0]),
-np.savetxt(name+"/m.dat",sol[:, 1]),
-np.savetxt(name+"/phi.dat",sol[:, 2]),
+plt.savefig(path+"/overview.png")
+np.savetxt(path+"/omega.dat",[omega]),
+np.savetxt(path+"/rvals.dat",r),
+np.savetxt(path+"/edelta.dat",1/sol[:, 0]),
+np.savetxt(path+"/m.dat",sol[:, 1]),
+np.savetxt(path+"/phi.dat",sol[:, 2]),
 
