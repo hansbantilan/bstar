edbe4ab72d59ba9bffe84e5bf984aa4ca7adedc6
Hans Bantilan <h.bantilan@qmul.ac.uk>
2020-02-12 13:05:58 +0000

    added lambda_search.py to update e_pow_minus_delta_guess while iteratively shooting for a solution with a target cosmological constant Lambda=Lend -HB

--- Code/star_solver_code/lambda_search.py
+++ Code/star_solver_code/lambda_search.py
@@ -5,7 +5,6 @@ import numpy as np
 import scipy.integrate as spi
 import scipy.optimize as opi
 import matplotlib
-matplotlib.use("agg")
 
 # =====================
 #  All imporntnat definitions
@@ -22,18 +21,27 @@ deltaR = 1
 N = 100000
 e_pow_minus_delta_guess = 0.4999
 
-verbose = 1
+verbose = 2
 eps = 1e-10  # Small epsilon to avoid r \neq 0
+
+Lstart =-0.2 #first cosmological constant Lambda
+Lend=-6.0    #target cosmological constant Lambda
+LN=59        #number of steps in Lambda
 
 # ====================================
 #   Main routine
 # ====================================
 
-pewpew = Complex_Boson_Star(e_pow_minus_delta_guess, phi0, D, Lambda, verbose)
+lambda_list=np.linspace(Lstart,Lend,LN)
 
-pewpew.print_parameters()
+for Lambda in lambda_list:
 
-alpha0 = pewpew.radial_walker(Rstart, Rend, deltaR, N, eps)
+	pewpew = Complex_Boson_Star(e_pow_minus_delta_guess, phi0, D, Lambda, verbose)
+	pewpew.print_parameters()
+
+	e_pow_minus_delta_guess = pewpew.radial_walker(Rstart, Rend, deltaR, N, eps)
+
+	print "For next iteration, now using\n e_pow_minus_delta_guess=",e_pow_minus_delta_guess
 
 
 # =====================================
